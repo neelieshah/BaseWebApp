e82ddb9ec72f6f8bc483dea04dba310136cc6083
neelieshah <neelie_shah@yahoo.com>
2019-02-23 14:12:38 -0500

    I added a few new pages and am trying to link them.

--- project/js/main.js
+++ project/js/main.js
@@ -3,9 +3,10 @@ function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
-  $("#demo").html("HIII I CHANGED THIS #javascript #fire");
+  $("#demo").html("LOOK AT ALL THESE PICS OF ME WOW <3");
+
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="images/wowdoge.jpeg" />`);
+  $("#neelie-image").append(`<img class="img-circle" src="images/neelie.jpeg" />`);
 }
 
--- project/start.js
+++ project/start.js
@@ -12,10 +12,16 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+app.get('/about-me', function(request, response) {
+  response.render('pages/about-me');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+
 
 
 // This file is what handles incoming requests and
